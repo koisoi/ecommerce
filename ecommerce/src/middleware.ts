f82a595694c0ff1d453067633e0b4d2bd3a99a21
koisoi <irisu.koi@gmail.com>
2024-03-27 15:09:18 +0300

    dynamic import fix

--- ecommerce/src/middleware.ts
+++ ecommerce/src/middleware.ts
@@ -1,8 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
-    // if (req.url === req.url.toLowerCase()) return NextResponse.next();
-
+    // console.log(`URL ${req.url} matches middleware!`);
     if (req.url !== req.url.toLowerCase())
         return NextResponse.redirect(
             new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase()),
@@ -13,5 +12,5 @@ export function middleware(req: NextRequest) {
 export const config = {
     // Skip all paths that should not be internationalized. This example skips the
     // folders "api", "_next" and all files with an extension (e.g. favicon.ico)
-    matcher: ["/((?!api|_next|.*\\..*).*)"]
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 };
